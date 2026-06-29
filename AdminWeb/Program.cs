de46d5f9856db1ec9d586960dec392bfcfd1cd6f
park0684 <parkjs0684@gmail.com>
2026-06-29 09:00:13 +0900

    Register scoped current access and menu services

--- AdminWeb/Program.cs
+++ AdminWeb/Program.cs
@@ -5,6 +5,8 @@ using poscam.AdminWeb.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
+const string currentUserAccessHttpClientName = "CurrentUserAccess";
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
@@ -12,6 +14,7 @@ builder.Services.Configure<ApiSettings>(
     builder.Configuration.GetSection("ApiSettings"));
 
 builder.Services.AddScoped<AuthStateService>();
+builder.Services.AddScoped<MenuAccessFilter>();
 
 builder.Services.AddHttpClient<ApiClient>((serviceProvider, client) =>
 {
@@ -21,6 +24,26 @@ builder.Services.AddHttpClient<ApiClient>((serviceProvider, client) =>
 
     client.BaseAddress = new Uri(apiSettings.BaseUrl);
 });
+
+// 현재 사용자 접근정보는 AuthServer를 호출하지만 기존 ApiClient와
+// HTTP 상태 처리 및 캐시 수명이 다르므로 별도 named client를 사용한다.
+builder.Services.AddHttpClient(currentUserAccessHttpClientName, (serviceProvider, client) =>
+{
+    var apiSettings = serviceProvider
+        .GetRequiredService<IOptions<ApiSettings>>()
+        .Value;
+
+    client.BaseAddress = new Uri(apiSettings.BaseUrl);
+});
+
+// AddHttpClient<T>의 기본 수명은 Transient이므로, 접근정보 캐시가
+// Blazor 회로의 Scoped 수명 동안 유지되도록 명시적으로 Scoped 등록한다.
+builder.Services.AddScoped(serviceProvider =>
+    new CurrentUserAccessService(
+        serviceProvider
+            .GetRequiredService<IHttpClientFactory>()
+            .CreateClient(currentUserAccessHttpClientName),
+        serviceProvider.GetRequiredService<AuthStateService>()));
 
 var app = builder.Build();
 
