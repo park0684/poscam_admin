a84c331421e1c4289694152d26755c63fa5d5d0d
park0684 <parkjs0684@gmail.com>
2026-07-02 23:48:55 +0900

    test: define partner permission codes

---
+++ AuthServer.Tests/Models/PartnerUserPermissionTypeTests.cs
@@ -0,0 +1,31 @@
+using poscam.AuthServer.Models.Enums;
+using Xunit;
+
+namespace poscam.AuthServer.Tests.Models;
+
+public class PartnerUserPermissionTypeTests
+{
+    [Fact]
+    public void PermissionCodes_UseSharedOperationalValues()
+    {
+        var expected = new Dictionary<PartnerUserPermissionType, int>
+        {
+            [PartnerUserPermissionType.PartnerUserManage] = 5,
+            [PartnerUserPermissionType.StoreManage] = 7,
+            [PartnerUserPermissionType.PartnerUserPasswordReset] = 9,
+            [PartnerUserPermissionType.ContractManage] = 10,
+            [PartnerUserPermissionType.LicenseManage] = 11,
+            [PartnerUserPermissionType.DeviceManage] = 13
+        };
+
+        var actual = Enum.GetValues<PartnerUserPermissionType>();
+
+        Assert.Equal(expected.Count, actual.Length);
+
+        foreach (var permission in actual)
+        {
+            Assert.True(expected.TryGetValue(permission, out var expectedCode));
+            Assert.Equal(expectedCode, (int)permission);
+        }
+    }
+}
