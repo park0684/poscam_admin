81691ebe0f5b3948c3e104e4da4de60cf9654dc9
park0684 <parkjs0684@gmail.com>
2026-07-03 03:36:34 +0900

    fix: remove partner password reset permission

--- AuthServer/Models/Enums/PartnerUserPermissionType.cs
+++ AuthServer/Models/Enums/PartnerUserPermissionType.cs
@@ -4,7 +4,6 @@ public enum PartnerUserPermissionType
 {
     PartnerUserManage = 5,
     StoreManage = 7,
-    PartnerUserPasswordReset = 9,
     ContractManage = 10,
     LicenseManage = 11,
     DeviceManage = 13
