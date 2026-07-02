e7b8b5a77f6f6e343ccc26b8fa0957978194cc55
park0684 <parkjs0684@gmail.com>
2026-07-02 23:44:10 +0900

    feat: define partner user permissions

---
+++ AuthServer/Models/Enums/PartnerUserPermissionType.cs
@@ -0,0 +1,11 @@
+namespace poscam.AuthServer.Models.Enums;
+
+public enum PartnerUserPermissionType
+{
+    PartnerUserManage = 5,
+    StoreManage = 7,
+    PartnerUserPasswordReset = 9,
+    ContractManage = 10,
+    LicenseManage = 11,
+    DeviceManage = 13
+}
