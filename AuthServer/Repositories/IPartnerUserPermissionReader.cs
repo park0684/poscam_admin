67eeb7b99a66d12500c75c660c9e2e8112300131
park0684 <parkjs0684@gmail.com>
2026-07-03 00:41:31 +0900

    feat: expose partner permission check contract

--- AuthServer/Repositories/IPartnerUserPermissionReader.cs
+++ AuthServer/Repositories/IPartnerUserPermissionReader.cs
@@ -1,6 +1,12 @@
+using poscam.AuthServer.Models.Enums;
+
 namespace poscam.AuthServer.Repositories;
 
 public interface IPartnerUserPermissionReader
 {
+    Task<bool> ExistsPermissionAsync(
+        int userCode,
+        PartnerUserPermissionType permission);
+
     Task<List<int>> GetPermissionCodesAsync(int userCode);
 }
