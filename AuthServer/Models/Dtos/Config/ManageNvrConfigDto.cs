48db88a48226a58039969fcbb9c8dcd38d7889d3
park0684 <parkjs0684@gmail.com>
2026-08-17 18:21:26 +0900

    feat: include NVR number in manage DTO

--- AuthServer/Models/Dtos/Config/ManageNvrConfigDto.cs
+++ AuthServer/Models/Dtos/Config/ManageNvrConfigDto.cs
@@ -7,10 +7,14 @@ namespace poscam.AuthServer.Models.Dtos.Config;
 ///
 /// 관리자 화면에서는 NVR 비밀번호를 직접 노출하지 않는다.
 /// 저장된 비밀번호가 있는지 여부만 표시한다.
-/// 구형 매장 상세 매핑 경로에서는 기존 운영값인 Dahua/RTSP 554를 기본값으로 사용한다.
 /// </summary>
 public class ManageNvrConfigDto
 {
+    /// <summary>
+    /// 매장 내부 NVR 번호.
+    /// </summary>
+    public int NvrNo { get; set; }
+
     public NvrProviderType NvrProvider { get; set; } = NvrProviderType.Dahua;
 
     public string NvrId { get; set; } = "";
